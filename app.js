3421918f475a093d37fac3082ba9ac748eac079d
Eduardo Villalobo <eduardovillalobo23@gmai.com>
2021-06-26 11:38:12 -0300

    modelos

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const {inquirerMenu}  = require('./helpers/inquirer');
-
 require('colors');
-console.clear();
+const {
+    inquirerMenu, 
+    pausa,
+    leerInput
+    }  = require('./helpers/inquirer');
+
+const Tareas = require('./models/tareas');
 
 const main = async ()=>{
-    console.log("Hola mundo");
-
+    
     let opt = '';
+
+    const tareas = new Tareas();
 
     do{
         opt = await inquirerMenu();
-        console.log({opt});
+        
+        switch (opt) {
+            case '1':
+                    const desc = await leerInput('Descripcion');
+                    tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log(tareas.listadoArr);
+                break;
+            default:
+                break;
+        }
+
+        await pausa();
+        
     }while(opt !== '0');
     
 }
